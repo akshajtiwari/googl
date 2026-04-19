2dd93d5ced2a311c015baad7f65ba19747f04206
Akshaj Tiwari <akshajtiwari9a@gmail.com>
2026-04-19 11:32:11 +0530

    update script

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Faster clone
+# Install Flutter (faster clone)
 git clone https://github.com/flutter/flutter.git -b stable --depth 1
 export PATH="$PATH:$(pwd)/flutter/bin"
 
+# Enable web
 flutter config --enable-web
+
+# Install dependencies
 flutter pub get
 
-# 🔥 reduce bundle size
-flutter build web --release --web-renderer canvaskit
+# Build web (FIXED)
+flutter build web --release
