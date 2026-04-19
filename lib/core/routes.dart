7fe64699b61bb1f8e9f4b7ca90639e629f098703
Aarush Srivastava <srivastavaaarush25@gmail.com>
2026-04-19 21:58:20 +0530

    teh settings tab is complete and all the tasks are now completed if any remaining or any unwanted feature that is to be removed or any alterations is to be done msg me (naa karna)

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -4,22 +4,25 @@ import '../screens/volunteers/volunteers_screen.dart';
 import '../screens/needs/needs_screen.dart';
 import '../screens/tasks/tasks_screen.dart';
 import '../screens/heatmap/heatmap.screen.dart';
-import '../screens/assingments/assignment_screen.dart'; // ← ADD
+import '../screens/assingments/assignment_screen.dart';
+import '../screens/settings/settings_screen.dart';
 
 class AppRoutes {
-  static const dashboard = '/';
-  static const volunteers = '/volunteers';
-  static const needs = '/needs';
-  static const tasks = '/tasks';
-  static const heatmap = '/heatmap';
-  static const assignments = '/assignments'; // ← ADD
+  static const dashboard   = '/';
+  static const volunteers  = '/volunteers';
+  static const needs       = '/needs';
+  static const tasks       = '/tasks';
+  static const heatmap     = '/heatmap';
+  static const assignments = '/assignments';
+  static const settings    = '/settings';
 
   static Map<String, WidgetBuilder> get routes => {
-        dashboard: (_) => const DashboardScreen(),
-        volunteers: (_) => const VolunteersScreen(),
-        needs: (_) => const NeedsScreen(),
-        tasks: (_) => const TasksScreen(),
-        heatmap: (_) => const HeatmapScreen(),
-        assignments: (_) => const AssignmentsScreen(), // ← ADD
-      };
+    dashboard:   (_) => const DashboardScreen(),
+    volunteers:  (_) => const VolunteersScreen(),
+    needs:       (_) => const NeedsScreen(),
+    tasks:       (_) => const TasksScreen(),
+    heatmap:     (_) => const HeatmapScreen(),
+    assignments: (_) => const AssignmentsScreen(),
+    settings:    (_) => const SettingsScreen(),
+  };
 }
